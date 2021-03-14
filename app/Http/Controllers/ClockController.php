89dcb84f0246a24a48ceb745f43e4846b1c16fe1
unknown <yapfeng@caritech.com>
2021-03-14 09:16:47 +0800

    added logout must with login condition

--- app/Http/Controllers/ClockController.php
+++ app/Http/Controllers/ClockController.php
@@ -47,36 +47,51 @@ class ClockController extends Controller
             ->first();
 
         if (!isset($db_attd) && empty($db_attd)) {
-            $db_attd = DB::table('attendances')->insert([
-                'user_id' => $db_user->id,
-                'clock' => request('clock_status'),
-                'mobile_date' => request('mobile_date'),
-                'mobile_time' => request('mobile_time'),
-                'longitude' => request('longitude'),
-                'latitude' => request('latitude'),
-                'timestamp' => request('timestamp'),
-                'accuracy' => request('accuracy'),
-                'altitude' => request('altitude'),
-                'heading' => request('heading'),
-                'speed' => request('speed'),
-                'speed_accuracy' => request('speed_accuracy'),
-                'is_mocked' => request('is_mocked'),
-                'house_no' => request('house_no'),
-                'street' => request('street'),
-                'country' => request('country'),
-                'postal_code' => request('postal_code'),
-                'state' => request('state'),
-                'city' => request('city'),
-                'area' => request('area'),
-                'street_name' => request('street_name'),
-                'created_at' => date('Y-m-d H:i:s'),
-            ]);
 
-            return response()->json([
-                'success' => true,
-                'type' => 'success',
-                'msg' => 'Clock ' . request('clock_status') . ' success',
-            ]);
+            $db_exist_in = DB::table('attendances')
+                ->where('user_id', $db_user->id)
+                ->where('mobile_date', request('mobile_date'))
+                ->where('clock', 'in')
+                ->first();
+
+            if (!isset($db_exist_in) && empty($db_exist_in) && request('clock_status') == 'out') {
+                return response()->json([
+                    'success' => false,
+                    'type' => 'error',
+                    'msg' => 'Please clock in first',
+                ]);
+            } else {
+                $db_attd = DB::table('attendances')->insert([
+                    'user_id' => $db_user->id,
+                    'clock' => request('clock_status'),
+                    'mobile_date' => request('mobile_date'),
+                    'mobile_time' => request('mobile_time'),
+                    'longitude' => request('longitude'),
+                    'latitude' => request('latitude'),
+                    'timestamp' => request('timestamp'),
+                    'accuracy' => request('accuracy'),
+                    'altitude' => request('altitude'),
+                    'heading' => request('heading'),
+                    'speed' => request('speed'),
+                    'speed_accuracy' => request('speed_accuracy'),
+                    'is_mocked' => request('is_mocked'),
+                    'house_no' => request('house_no'),
+                    'street' => request('street'),
+                    'country' => request('country'),
+                    'postal_code' => request('postal_code'),
+                    'state' => request('state'),
+                    'city' => request('city'),
+                    'area' => request('area'),
+                    'street_name' => request('street_name'),
+                    'created_at' => date('Y-m-d H:i:s'),
+                ]);
+
+                return response()->json([
+                    'success' => true,
+                    'type' => 'success',
+                    'msg' => 'Clock ' . request('clock_status') . ' success',
+                ]);
+            }
         } elseif (isset($db_attd) && !empty($db_attd)) {
             return response()->json([
                 'success' => false,
@@ -98,6 +113,7 @@ class ClockController extends Controller
             $db_attd = DB::table('attendances')
                 ->select('clock', 'mobile_date', 'mobile_time')
                 ->where('user_id', $db_user->id)
+                ->orderBy('id', 'DESC')
                 ->get();
 
             return response()->json([
